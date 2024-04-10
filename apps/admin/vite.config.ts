9d52d69c28a8c9b98d295102e06a4fcc827e3c7d
JackDeng666 <1556393081@qq.com>
2024-04-10 14:48:01 +0800

    refactor: add vite-tsconfig-paths

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,16 +13,10 @@ export default defineConfig({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       // 指定symbolId格式
       symbolId: 'icon-[dir]-[name]'
-    })
+    }),
+    tsconfigPaths()
   ],
   base: '/admin/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '#': path.resolve(__dirname, './types'),
-      '@shared': path.resolve(__dirname, '../server/src/shared')
-    }
-  },
   server: {
     // open: true,
     host: true,
