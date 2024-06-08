9264c3c5f9b175a5a022917712bfd4fe9e0e8f48
JackDeng666 <1556393081@qq.com>
2024-06-08 16:06:09 +0800

    fix(admin): add ListFileDto

--- apps/admin/src/api/file.ts
+++ apps/admin/src/api/file.ts
@@ -1,3 +1,4 @@
+import { ListFileDto } from '@ying/shared'
 import { FileEntity } from '@ying/shared/entities'
 import { request } from './request'
 
@@ -5,4 +6,12 @@ export function upload(file: File): Promise<FileEntity> {
   const form = new FormData()
   form.append('file', file)
   return request.post('/file', form)
+}
+
+export function list(params: ListFileDto): Promise<FileEntity[]> {
+  return request.get('/file/list', { params })
+}
+
+export function listCount(params: ListFileDto): Promise<number> {
+  return request.get('/file/list-count', { params })
 }
--- libs/shared/src/admin/dto/index.ts
+++ libs/shared/src/admin/dto/index.ts
@@ -1,6 +1,6 @@
 export * from './auth.dto'
 export * from './list.dto'
-export * from './sys.permission.dto'
 export * from './sys.role.dto'
 export * from './sys.user.dto'
 export * from './user.dto'
+export * from './file.dto'
